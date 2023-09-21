9a0c7912128270cb655b89ef28c85da7f54bcc56
KSKushal123 <133742980+KSKushal123@users.noreply.github.com>
2023-09-21 11:56:05 +0530

    Add files via upload

---
+++ data structure.py
@@ -0,0 +1,28 @@
+numberl = 100
+print("The type of number1 ",type(numberl))
+integer_1 = 100
+integer_2 = 50
+print (integer_1*integer_2)
+print(integer_1+integer_2)
+print(integer_1-integer_2)
+print(integer_1/integer_2)
+number2 = 100.0
+print("The type of number2 ",type(number2))
+float_1 = 12.539
+float_2 = 6.78
+print (float_1*float_2)
+print(float_1+float_2)
+print(float_1-float_2)
+print (float_1/float_2)
+str = 'Jack Daniels'
+print("The type of str ",type(str))
+str_1 = 'Hello'
+str_2 = 'World'
+print (str_1 + str_2)
+result = 100 < 200
+print("The type of result ",type(result))
+has_passed = False
+marks = 80
+if (marks > 50):
+    has_passed = True
+print(has_passed)
